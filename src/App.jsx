42cb915ba7778ff52b9d157efa8b50b449efb1d6
unknown <jangjaeko45@gmail.com>
2025-11-25 23:15:16 -0800

    feat : content create logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "./component/Header.jsx";
 import Editor from "./component/Editor.jsx";
 import List from "./component/List.jsx";
@@ -6,10 +6,37 @@ import List from "./component/List.jsx";
 import "./App.css";
 
 function App() {
+  const mockData = [
+    {
+      id: 0,
+      isDone: false,
+      content: "react",
+      date: new Date().getTime(),
+    },
+    {
+      id: 1,
+      isDone: false,
+      content: "learn singing",
+      date: new Date().getTime(),
+    },
+  ];
+  const [toDos, setToDos] = useState([mockData]);
+  const idRef = useRef(2);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setToDos([newTodo, ...toDos]);
+  };
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
--- src/component/Editor.jsx
+++ src/component/Editor.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import "./Editor.css";
-export default function Editor() {
+import { useState, useRef } from "react";
+export default function Editor({ onCreate }) {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+  const okKeyDown = (e) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
   return (
     <div className="Editor">
-      <input placeholder="new Todo ...." />
-      <button>Add</button>
+      <input
+        onKeyDown={okKeyDown}
+        ref={contentRef}
+        value={content}
+        onChange={onChangeContent}
+        placeholder="new Todo ...."
+      />
+      <button onClick={onSubmit}>Add</button>
     </div>
   );
 }
